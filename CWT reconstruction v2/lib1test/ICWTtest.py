13256375d9825085a229e92bd4d3e36527eeb770
Anton <mr.highavail@gmail.com>
2024-03-23 09:51:45 +0200

    add new cases

---
+++ CWT reconstruction v2/lib1test/ICWTtest.py
@@ -0,0 +1,6 @@
+# cd "./CWT reconstruction v2/lib1test/"
+#C:\Python34\python.exe -m venv ./
+# .\Scripts\activate
+# pip install numpy==1.7
+# pip install scipy==0.11
+# pip install git+https://github.com/aaren/wavelets
