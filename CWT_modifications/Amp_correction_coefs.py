fluctuations by noise fix using mean removal and normalisation update

CWT_modifications/Amp_correction_coefs.py
@@ -2,18 +2,19 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
-def def_window(val1,val2):
+def def_window(val1,val2, q_factor = 1):
     if val1 == 0 and val2==0:
         return 0
     diff_cof = min(abs(val1), abs(val2)) / max(abs(val1), abs(val2))
-    diff_cof = diff_cof ** 1
+    diff_cof = diff_cof ** q_factor
     return diff_cof
 
 def rectangular_window(val1,val2,min_bound = 0.8, max_bound = 1.2):
     if val1 == 0 and val2==0:
         diff_cof = 0
     else:
-        diff_cof = min(abs(val1), abs(val2)) / max(abs(val1), abs(val2))
+        # diff_cof = min(abs(val1), abs(val2)) / max(abs(val1), abs(val2))
+        diff_cof = abs(val1)/abs(val2)
     if diff_cof>min_bound and diff_cof<max_bound:
         return 1
     else:
@@ -23,11 +24,18 @@ def tukey_window(val1,val2, min_bound = 0.8, max_bound = 1.2,alpha = 0.5):
     if val1 == 0 and val2==0:
         diff_cof = 0
     else:
-        diff_cof = min(abs(val1), abs(val2)) / max(abs(val1), abs(val2))
+        # diff_cof = min(abs(val1), abs(val2)) / max(abs(val1), abs(val2))
+        diff_cof = abs(val1)/abs(val2)
     n_counts = 100
     shift_vector = np.arange(min_bound,max_bound, (max_bound-min_bound)/n_counts)
     window = signal.windows.tukey(n_counts, alpha=alpha)
-    window_correct_coef = window[np.where(shift_vector>diff_cof)][0]
+    # print(diff_cof)
+    # print(window)
+    # input(shift_vector)
+    try:
+        window_correct_coef = window[np.where(shift_vector>diff_cof)][0]
+    except:
+        window_correct_coef = 0
     diff_cof = window_correct_coef
     # plt.plot(shift_vector,window)
     # plt.title("Tukey window")
@@ -70,21 +78,30 @@ def tukey_window(val1,val2, min_bound = 0.8, max_bound = 1.2,alpha = 0.5):
 #     return cos_similarity_result
 #
 
-def normalized_cosine_similarity(vector1, vector2):
+def normalized_cosine_similarity(sig_part, wavelet_part, target_value):
+    sigpart_mean = np.mean(sig_part)
+    # sigpart_mean = 0
+
     dot_prod = 0
     norm_v1 = 0
     norm_v2 = 0
 
-    for val1, val2 in zip(vector1, vector2):
+    diff_cof_mass =[]
 
-        diff_cof = def_window(val1,val2)
-        # diff_cof = rectangular_window(val1,val2, min_bound=0.5, max_bound= 2)
-        # diff_cof = tukey_window(val1,val2,min_bound= 0.5,max_bound= 2)
+    for val1, val2 in zip(sig_part, wavelet_part):
+
+        diff_cof = def_window(val1-sigpart_mean, target_value, q_factor = 2)
+        # diff_cof = 1
+        # diff_cof = rectangular_window(val1-sigpart_mean,target_value, min_bound=0.5, max_bound= 2)
+
+        # diff_cof = tukey_window(val1-sigpart_mean,target_value, min_bound=0.8, max_bound= 1.2)
+        diff_cof_mass.append(diff_cof)
 
         # Use the complex conjugate of val1 (from complex scalar product)
-        dot_prod += (val1 * np.conj(val2))*diff_cof
+        dot_prod += (val1 * np.conj(val2))#*diff_cof
         norm_v1 += np.abs(val1) ** 2
         norm_v2 += np.abs(val2) ** 2
+    # return diff_cof_mass
 
     # Avoid division by zero by checking norms before division
     if np.sqrt(norm_v1) == 0 or np.sqrt(norm_v2) == 0:
@@ -92,4 +109,13 @@ def normalized_cosine_similarity(vector1, vector2):
     else:
         cos_similarity_result = dot_prod / (np.sqrt(norm_v1) * np.sqrt(norm_v2))
 
-    return cos_similarity_result
+    return cos_similarity_result*np.mean(diff_cof_mass)
+
+def dot_prod(v1,v2):
+    dot_prod = 0
+
+
+    for val1, val2 in zip(v1, v2):
+        dot_prod += (val1 * np.conj(val2))
+
+    return dot_prod

CWT_modifications/CWT_mod1.py
@@ -1,6 +1,8 @@
+import copy
+
 import numpy as np
 import matplotlib.pyplot as plt
-from Convolve_mod import convolve_same2, custom_conv_with_metric, convolve_cosine_sim_based_mod
+from Convolve_mod import convolve_same2, custom_conv_with_metric, convolve_cosine_sim_based_mod, convolve_mod
 def morlet_wavelet(t, w=6.0):
     wavelet = np.exp(1j * w * t) * np.exp(-0.5 * t ** 2) * np.pi ** (-0.25)
     return wavelet
@@ -88,10 +90,9 @@ def cwt(signal, scales, wavelet_function, dt=1.0, fs = 100, w0 = 6, plot_wavelet
 
         # -----------------mod----------------#
         wavelet_data = cut_wavelet(wavelet_data)
-
+        wavelet_data_old = copy.deepcopy(wavelet_data)
         # scale wavelet_to_targe
         wavelet_data = (wavelet_data/max(wavelet_data))*Amp_correction_target_amp
-        # scale_correction = max(wavelet_data)/Amp_correction_target_amp
         # scale_corrections_mass.append(scale_correction)
         # -------------------------------------
 
@@ -108,7 +109,8 @@ def cwt(signal, scales, wavelet_function, dt=1.0, fs = 100, w0 = 6, plot_wavelet
         # output[i, :] = convolve_same2(signal, np.conj(wavelet_data))
         # output[i, :] = custom_conv_with_metric(signal, np.conj(wavelet_data))
 
-        output[i, :] = convolve_cosine_sim_based_mod(signal, np.conj(wavelet_data))
+        output[i, :] = convolve_cosine_sim_based_mod(signal, np.conj(wavelet_data),Amp_correction_target_amp)*max(wavelet_data_old)
+        # output[i, :] = convolve_mod(signal, np.conj(wavelet_data_old))
 
     # -----Plot Wavelet spectrums----#
     if plot_wavelets_spectrum:

CWT_modifications/Convolve_mod.py
@@ -1,4 +1,6 @@
-from Amp_correction_coefs import def_window,rectangular_window,tukey_window,normalized_cosine_similarity
+import matplotlib.pyplot as plt
+
+from Amp_correction_coefs import def_window,rectangular_window,tukey_window,normalized_cosine_similarity,dot_prod
 import numpy as np
 
 
@@ -37,7 +39,7 @@ def convolve_same2(a, b):
     return result[start:end]
 
 
-def convolve_cosine_sim_based_mod(sig, wavelet):
+def convolve_cosine_sim_based_mod(sig, wavelet, target_value):
     if len(wavelet)%2 != 0:
         wavelet = wavelet[1:]
     wavle_half_pos = int(np.round(len(wavelet)/2))
@@ -52,11 +54,46 @@ def convolve_cosine_sim_based_mod(sig, wavelet):
         stop_sig_point = min(i + wavle_half_pos , len(sig))
         compared_part = sig[start_sig_point:stop_sig_point]
 
-
-        similarity_results_mass.append(normalized_cosine_similarity(compared_part,wavelet_part))
+        # plt.figure()
+        # plt.title((str(normalized_cosine_similarity(compared_part,wavelet_part))))
+        # plt.subplot(2,1,1)
+        # plt.plot(compared_part)
+        # plt.subplot(2,1,2)
+        # plt.plot(wavelet_part)
+        # plt.show()
+        #
+        # similarity_result = normalized_cosine_similarity(compared_part,wavelet_part)
+        # plt.figure()
+        # plt.subplot(3,1,1)
+        # plt.plot(compared_part)
+        # plt.subplot(3, 1, 2)
+        # plt.plot(wavelet_part)
+        # plt.subplot(3, 1, 3)
+        # plt.plot(similarity_result)
+        # plt.show()
+        similarity_results_mass.append(normalized_cosine_similarity(compared_part,wavelet_part, target_value))
         # similarity_results_mass[i] = normalized_cosine_similarity(compared_part, np.real(wavelet_part))
 
-    return similarity_results_mass
+    return np.array(similarity_results_mass)
+
+def convolve_mod(sig, wavelet):
+    if len(wavelet)%2 != 0:
+        wavelet = wavelet[1:]
+    wavle_half_pos = int(np.round(len(wavelet)/2))
+
+    conv_mass = []
+    for i in range(len(sig)):
+        wavelet_start_pos = max(wavle_half_pos-i, 0)
+        wavelet_end_pos = min(wavle_half_pos + (len(sig) - i),len(wavelet))
+        wavelet_part = wavelet[wavelet_start_pos:wavelet_end_pos]
+
+        start_sig_point = max(i - wavle_half_pos, 0)
+        stop_sig_point = min(i + wavle_half_pos , len(sig))
+        compared_part = sig[start_sig_point:stop_sig_point]
+
+        conv_mass.append(dot_prod(compared_part,wavelet_part))
+
+    return np.array(conv_mass)
 
 
 

CWT_modifications/cwt_test1_artif_LP_sig.py
@@ -4,24 +4,34 @@ import matplotlib.pyplot as plt
 # from CWT_basis import cwt,icwt,morlet_wavelet,scale_to_frequency
 from CWT_mod1 import cwt,icwt,morlet_wavelet,scale_to_frequency
 import numpy as np
+from AnalogFilters import AnalogFilterDesign
 
 
 if __name__ == '__main__':
     w0 = 6
-    fs = 500
+    fs = 1000
     sig = simulate_ecg_with_VLP_ALP(duration = 4, #sec
                                   fs = fs, #hz
-                                  noise_level =130, #db
+                                  noise_level =40, #db
                                   hr = 80,#bpm
-                                  Std = 2, #bpm
+                                  Std = 0, #bpm
                                   unregular_comp = False,
                                     random_state = 11,
-                                  lap_amp = 10,
-                                  lvp_amp = 30)
+                                  lap_amp = 25,
+                                  lvp_amp = 25)
+
+
 
     # sig = sig[120:600]
     # sig = sig[240:1200]
-    sig = sig[280:480]
+    # sig = sig[280:700]
+    sig = sig[560:1400]
+
+
+
+
+
+
 
     # sig = sigTotest(fs=fs)
     # print(len(sig))
@@ -29,7 +39,21 @@ if __name__ == '__main__':
     t_vector = np.array(list(range(len(sig))))/fs
 
 
-    scales = np.linspace(3, 250, 100)
+    # scales = np.linspace(2, 250, 100)
+
+    scales = np.linspace(3, 40, 40)
+
+    # def overlap_50_scales(from_scale, num_scales):
+    #     result_scales = []
+    #     n_mass = np.array(list(range(num_scales)))
+    #     for n in n_mass:
+    #         result_scales.append(from_scale * ((2 ** 0.5) ** n))
+    #     return np.array(result_scales)
+    #
+    #
+    # scales = overlap_50_scales(3, 12)
+
+
     cwt_coefficients = cwt(sig, scales, morlet_wavelet, dt=1, fs=fs,w0=6, plot_wavelets_spectrum=False)
     reconstructed_signal = icwt(cwt_coefficients, scales, morlet_wavelet, dt=1, ds=1)
 

examples/testlibs/Customize_Algo.py
@@ -189,9 +189,8 @@ def cwt_customized(signal, scales):
         # print(len(custom_cwt_matrix[ind, :]))
         # input(custom_cwt_matrix[ind, :])
 
-        custom_cwt_matrix[ind, :] = convolve_cosine_sim_based_mod(signal, wavelet_body)
-        print(len(custom_cwt_matrix[ind, :]))
-        input(custom_cwt_matrix[ind, :])
+        # custom_cwt_matrix[ind, :] = convolve_cosine_sim_based_mod(signal, wavelet_body)
+
     # plt.figure()
     # X, Y = np.meshgrid(list(range(len(signal))), scales)
     # plt.pcolormesh(X, Y, np.abs(custom_cwt_matrix), cmap='viridis')
@@ -214,7 +213,7 @@ fs = 500
 sig = sigTotest(fs=fs, part_t=0.1)
 t = np.arange(0, len(sig)/fs, 1/fs)
 num_scales = 80
-scales = np.arange(0.1, num_scales,0.1)
+scales = np.arange(3, num_scales,1)
 
 
 # omega0 = 5